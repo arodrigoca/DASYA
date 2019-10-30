2c3f6851283bbc57a9221052cd99eb22c2954030
arodrigoca <adri_rodri_64@hotmail.es>
2019-10-30 22:49:16 +0100

    final version

--- go/src/quijote/quijote.go
+++ go/src/quijote/quijote.go
@@ -2,63 +2,62 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
-	"strings"
-	"unicode"
-	"flag"
 	"sort"
 	"strconv"
+	"strings"
+	"unicode"
 )
 
-type wordInfo struct{
+type wordInfo struct {
 	timesAppeared int
-	seenInLines []int
-
+	seenInLines   []int
 }
 
-func cleanWord(word string) string{
+func CleanWord(word string) string {
 
-		//fmt.Printf("Input: %s\n", word)
-    runes := []rune(word)
-		i := 0
-		for{
-			if len(runes) >= 1 && !unicode.IsLetter(runes[i]){
-				runes = runes[i+1:]
-			}else{
-				break
-			}
+	//fmt.Printf("Input: %s\n", word)
+	runes := []rune(word)
+	i := 0
+	for {
+		if len(runes) >= 1 && !unicode.IsLetter(runes[i]) {
+			runes = runes[i+1:]
+		} else {
+			break
 		}
-		//fmt.Printf("Output: %s\n", string(runes))
-    return  string(runes)
+	}
+	//fmt.Printf("Output: %s\n", string(runes))
+	return string(runes)
 }
 
-func saveTargetWords(splittedWords []string, wordsDictionary map[string]wordInfo, lineCounter int) {
+func SaveTargetWords(splittedWords []string, wordsDictionary map[string]wordInfo, lineCounter int) {
 
 	for i := 0; i < len(splittedWords); i++ {
-		if (strings.Contains(splittedWords[i], ".") || strings.Contains(splittedWords[i], ":")) && i != len(splittedWords)-1{
+		if (strings.Contains(splittedWords[i], ".") || strings.Contains(splittedWords[i], ":")) && i != len(splittedWords)-1 {
 			targetWord := splittedWords[i+1]
 			if runes := []rune(targetWord); unicode.IsLetter(runes[len(runes)-1]) == false {
 				targetWord = targetWord[:len(targetWord)-1]
 			}
-      	targetWord = cleanWord(targetWord)
-				if len(targetWord) >= 3{
-					insertInMap(wordsDictionary, targetWord, lineCounter)
-				}
+			targetWord = CleanWord(targetWord)
+			if len(targetWord) >= 3 {
+				insertInMap(wordsDictionary, targetWord, lineCounter)
+			}
 		}
 	}
 }
 
 func insertInMap(wordsDictionary map[string]wordInfo, targetWord string, lineCounter int) {
 
-		targetWord = strings.ToLower(targetWord)
-		linesSlice := wordsDictionary[targetWord].seenInLines
-		if idx:=sort.SearchInts(linesSlice, lineCounter); idx<len(linesSlice) && len(linesSlice)!=0{
-			wordsDictionary[targetWord] = wordInfo{wordsDictionary[targetWord].timesAppeared + 1, linesSlice}
+	targetWord = strings.ToLower(targetWord)
+	linesSlice := wordsDictionary[targetWord].seenInLines
+	if idx := sort.SearchInts(linesSlice, lineCounter); idx < len(linesSlice) && len(linesSlice) != 0 {
+		wordsDictionary[targetWord] = wordInfo{wordsDictionary[targetWord].timesAppeared + 1, linesSlice}
 
-		}else{
-			wordsDictionary[targetWord] = wordInfo{wordsDictionary[targetWord].timesAppeared + 1, append(wordsDictionary[targetWord].seenInLines, lineCounter)}
-		}
+	} else {
+		wordsDictionary[targetWord] = wordInfo{wordsDictionary[targetWord].timesAppeared + 1, append(wordsDictionary[targetWord].seenInLines, lineCounter)}
+	}
 }
 
 func ScanAndProcess(scanner *bufio.Scanner, wordsDictionary map[string]wordInfo) {
@@ -67,33 +66,33 @@ func ScanAndProcess(scanner *bufio.Scanner, wordsDictionary map[string]wordInfo)
 	for scanner.Scan() {
 		line := scanner.Text()
 		splittedWords := strings.Fields(line)
-		saveTargetWords(splittedWords, wordsDictionary, lineCounter)
+		SaveTargetWords(splittedWords, wordsDictionary, lineCounter)
 		lineCounter++
 	}
 
 }
 
-func parseArguments() string{
+func parseArguments() string {
 
 	filenamePtr := flag.String("file", "", "filename to read")
 	flag.Parse()
-	if *filenamePtr == ""{
-			fmt.Println("Error: at least argument -file is necessary.")
-			os.Exit(1)
+	if *filenamePtr == "" {
+		fmt.Println("Error: at least argument -file is necessary.")
+		os.Exit(1)
 	}
 
 	return *filenamePtr
 }
 
-func PrettyPrint(wordsDictionary map[string]wordInfo, filename string){
+func PrettyPrint(wordsDictionary map[string]wordInfo, filename string) {
 
 	for i, v := range wordsDictionary {
 
-        fmt.Printf("%s:%s\n", i, strconv.Itoa(v.timesAppeared))
-				for i:=0; i<len(v.seenInLines); i++{
-					fmt.Println("\t" + filename + ":" + strconv.Itoa(v.seenInLines[i]))
-				}
-  }
+		fmt.Printf("%s:%s\n", i, strconv.Itoa(v.timesAppeared))
+		for i := 0; i < len(v.seenInLines); i++ {
+			fmt.Println("\t" + filename + ":" + strconv.Itoa(v.seenInLines[i]))
+		}
+	}
 }
 
 func main() {
--- go/src/quijote/quijote_white_box_test.go
+++ go/src/quijote/quijote_white_box_test.go
@@ -1,31 +1,30 @@
 package main
 
-import ("testing"
-        "strings"
-        "bufio"
+import (
+	"bufio"
+	"strings"
+	"testing"
 )
 
+func TestCleanWord(t *testing.T) {
 
-func TestQuijote(t *testing.T){
-
-  scanner := bufio.NewScanner(strings.NewReader("Hola. Saludos. fsadf;lkjsbdl;3214: qu . ggfßðfg®äåé ðf f : ßðfß"))
-  var testDictionary map[string]wordInfo
-	testDictionary = make(map[string]wordInfo)
-  ScanAndProcess(scanner, testDictionary)
-  if len(testDictionary) < 4{
-    t.Errorf("program failed, returned %v", testDictionary)
-  }
+	result := CleanWord("``. .«»¡²³¤€¼½:hola")
+	if result != "hola" {
+		t.Logf("CleanWord failed, returned %s", result)
+	} else {
+		t.Logf("CleanWord suceeded, returned %s", result)
+	}
 
 }
 
-func TestQuijoteVoid(t *testing.T){
+func TestQuijote(t *testing.T) {
 
-  scanner := bufio.NewScanner(strings.NewReader(""))
-  var testDictionary map[string]wordInfo
+	scanner := bufio.NewScanner(strings.NewReader(".Hola. Saludos. .fsadf;lkjsbdl;3214: qu . :ggfßðfg®äåé: ðf f : ßðfß"))
+	var testDictionary map[string]wordInfo
 	testDictionary = make(map[string]wordInfo)
-  ScanAndProcess(scanner, testDictionary)
-  if len(testDictionary) > 0{
-    t.Errorf("program failed, returned %v", testDictionary)
-  }
+	ScanAndProcess(scanner, testDictionary)
+	if len(testDictionary) < 4 {
+		t.Errorf("program failed, returned %v", testDictionary)
+	}
 
 }
